8b9895093b7bdd06ea49709ed276dc05f577040e
Hector Montero <montero.hector@gmail.com>
2016-12-22 11:05:54 +0100

    angular version downgrade to 1.59 that support $http service in angular core.

--- spec/omdb/service.spec.js
+++ spec/omdb/service.spec.js
@@ -5,6 +5,7 @@ describe('omdb service', function(){
     var movieDataById = {"Title":"Star Wars","Year":"1983","Rated":"N/A","Released":"01 May 1983","Runtime":"N/A","Genre":"Action, Adventure, Sci-Fi","Director":"N/A","Writer":"N/A","Actors":"Harrison Ford, Alec Guinness, Mark Hamill, James Earl Jones","Plot":"In this game, you assume the role of Luke Skywalker and fight past many enemies to to reach and destroy the Death Star.","Language":"English","Country":"USA","Awards":"N/A","Poster":"https://images-na.ssl-images-amazon.com/images/M/MV5BMWJhYWQ3ZTEtYTVkOS00ZmNlLWIxZjYtODZjNTlhMjMzNGM2XkEyXkFqcGdeQXVyNzg5OTk2OA@@._V1_SX300.jpg","Metascore":"N/A","imdbRating":"7.8","imdbVotes":"368","imdbID":"tt0251413","Type":"game","Response":"True"};  
      
     var omdbApi = {};
+    var $httpBackend; 
     
     /* 
         First we load the whole module that contains the service we want to test
@@ -15,16 +16,26 @@ describe('omdb service', function(){
     /* 
         Inject the specific service to be tested. 
     */
-    beforeEach(inject(function(_omdbApi_){
-        omdbApi = _omdbApi_; 
+    beforeEach(inject(function(_omdbApi_, _$httpBackend_){
+        omdbApi = _omdbApi_;
+        $httpBackend = _$httpBackend_;  
     }));
 
     /*
         What is expected to happen when a search is issued? 
     */
     it('should return movie data from a search', function(){
-        console.log(dump(movieData));
-        expect(omdbApi.search('Star Wars')).toEqual(movieData);
+        //console.log(dump(movieData));
+        var response;
+        var expectedUrl = 'http://www.omdbapi.com/?v=1&s=star%20wars';
+
+        $httpBackend.when('GET', expectedUrl).respond(200, movieData);
+        omdbApi.search('star wars')
+                .then(function(data){   
+                    response = data; 
+                }); 
+        $httpBackend.flush();
+        expect(response).toEqual(movieData);
     });
 
 
@@ -32,7 +43,18 @@ describe('omdb service', function(){
         What is expected to happen when a search is issued? 
     */
     it('should return movie data by id', function(){
-        expect(omdbApi.find('tt0251413')).toEqual(movieDataById);
+        var response; 
+        var expectedUrl = 'http://www.omdbapi.com/?v=1&i=tt0251413';
+        
+        $httpBackend.when('GET', expectedUrl).respond(200, movieDataById);
+
+        omdbApi.find('tt0251413').then(function(data){   
+                response = data; 
+            });
+
+        $httpBackend.flush();
+
+        expect(response).toEqual(movieDataById);
     }); 
     
 }); 
--- src/omdb/service.js
+++ src/omdb/service.js
@@ -1,14 +1,27 @@
 angular.module('omdb', [])
-    .factory('omdbApi', function(){
+    .factory('omdbApi', function($http, $q){
         var service = {};
+        var baseUrl = 'http://www.omdbapi.com/?v=1&';
 
-        service.search = function(query){            
-            return {"Title":"Star Wars","Year":"1983","Rated":"N/A","Released":"01 May 1983","Runtime":"N/A","Genre":"Action, Adventure, Sci-Fi","Director":"N/A","Writer":"N/A","Actors":"Harrison Ford, Alec Guinness, Mark Hamill, James Earl Jones","Plot":"In this game, you assume the role of Luke Skywalker and fight past many enemies to to reach and destroy the Death Star.","Language":"English","Country":"USA","Awards":"N/A","Poster":"https://images-na.ssl-images-amazon.com/images/M/MV5BMWJhYWQ3ZTEtYTVkOS00ZmNlLWIxZjYtODZjNTlhMjMzNGM2XkEyXkFqcGdeQXVyNzg5OTk2OA@@._V1_SX300.jpg","Metascore":"N/A","imdbRating":"7.8","imdbVotes":"368","imdbID":"tt0251413","Type":"game","Response":"True"};
+        service.search = function(query){
+            var deferred = $q.defer();
+            $http.get(baseUrl + 's=' + encodeURIComponent(query))
+                .success(function(data){                    
+                    deferred.resolve(data);
+                });
+            return deferred.promise; 
         };
 
-
+        // service.find = function(id){
+        //     return {"Title":"Star Wars","Year":"1983","Rated":"N/A","Released":"01 May 1983","Runtime":"N/A","Genre":"Action, Adventure, Sci-Fi","Director":"N/A","Writer":"N/A","Actors":"Harrison Ford, Alec Guinness, Mark Hamill, James Earl Jones","Plot":"In this game, you assume the role of Luke Skywalker and fight past many enemies to to reach and destroy the Death Star.","Language":"English","Country":"USA","Awards":"N/A","Poster":"https://images-na.ssl-images-amazon.com/images/M/MV5BMWJhYWQ3ZTEtYTVkOS00ZmNlLWIxZjYtODZjNTlhMjMzNGM2XkEyXkFqcGdeQXVyNzg5OTk2OA@@._V1_SX300.jpg","Metascore":"N/A","imdbRating":"7.8","imdbVotes":"368","imdbID":"tt0251413","Type":"game","Response":"True"}; 
+        // }
         service.find = function(id){
-            return {"Title":"Star Wars","Year":"1983","Rated":"N/A","Released":"01 May 1983","Runtime":"N/A","Genre":"Action, Adventure, Sci-Fi","Director":"N/A","Writer":"N/A","Actors":"Harrison Ford, Alec Guinness, Mark Hamill, James Earl Jones","Plot":"In this game, you assume the role of Luke Skywalker and fight past many enemies to to reach and destroy the Death Star.","Language":"English","Country":"USA","Awards":"N/A","Poster":"https://images-na.ssl-images-amazon.com/images/M/MV5BMWJhYWQ3ZTEtYTVkOS00ZmNlLWIxZjYtODZjNTlhMjMzNGM2XkEyXkFqcGdeQXVyNzg5OTk2OA@@._V1_SX300.jpg","Metascore":"N/A","imdbRating":"7.8","imdbVotes":"368","imdbID":"tt0251413","Type":"game","Response":"True"}; 
+            var deferred = $q.defer(); 
+            $http.get(baseUrl + 'i=' + id)
+                .success(function(data){
+                    deferred.resolve(data);
+                }); 
+            return deferred.promise;
         }
 
         return service;
