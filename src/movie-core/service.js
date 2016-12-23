07a55b72aecbc7052a7e97607c100ddd8ec60fda
Hector Montero <montero.hector@gmail.com>
2016-12-23 12:10:05 +0100

    test that authtoken is being sent in each one the crud request.

--- src/movie-core/service.js
+++ src/movie-core/service.js
@@ -1,10 +1,28 @@
 angular
     .module('MovieCore', ['ngResource'])
     .factory('PopularMovies', function($resource){
-
+        var token = 'supersecrettoken'; 
+        
         return $resource('popular/:movieId', { movieId: '@id' }, {
             update: {
-                method: 'PUT'
+                method: 'PUT', 
+                headers: { 'authToken': token }
+            }, 
+            get: {
+                method: 'GET',
+                headers: { 'authToken': token }
+            },
+            query: {
+                method: 'GET',
+                headers: { 'authToken': token }
+            },
+            save: {
+                method: 'POST',
+                headers: { 'authToken': token }
+            },
+            remove: {
+                method: 'DELETE',
+                headers: { 'authToken': token }
             }
         });
     }); 
